85397819430878475f017e7329d742a503bbede6
Googmusic <brenton.chaplin69@gmail.com>
2026-06-01 18:34:10 +0930

    Implement service worker caching for gig player

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+const CACHE = "gig-player-v1";
 
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache=>{
+      return cache.addAll(["./"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=> res || fetch(e.request))
+  );
+});
